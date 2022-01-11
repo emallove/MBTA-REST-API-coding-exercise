ec37753756f8bf6f1be22927d1fd55df36e7179b
Ethan Mallove <emallove@yahoo.com>
2022-01-11 00:41:56 -0500

    Complete question 2, URL formatting, stops-routes mappings, use f"{}" strings
     * Format URLs so that each &param=val is on one line
     * Gather stops-routes mappings using /trips API
     * Move code inline with Question document
     * Use api key, and f"{foo}..." string formatting for URLs

--- a.py
+++ a.py
@@ -4,6 +4,11 @@ import requests
 import json
 from pprint import pprint
 
+api_key = "api_key=d2e03097f9c24dcfa494d95f9f3c3b81"
+base_url = "https://api-v3.mbta.com"
+route_types_filter = "0,1"
+
+
 #
 # Boston's transportation system, the MBTA (https://mbta.com/), has a website with APIs
 # https://api-v3.mbta.com/docs/swagger/index.html.
@@ -18,6 +23,7 @@ from pprint import pprint
 # client code to interact directly with the APIs is most likely easier, and better demonstrates the
 # skills we're interested in reviewing.
 #
+
 #####################################################################################################
 # Question 1
 #
@@ -30,10 +36,38 @@ from pprint import pprint
 #
 # 1. Download all results from https://api-v3.mbta.com/routes then filter locally
 # 2. Rely on the server API (i.e., https://api-v3.mbta.com/routes?filter[type]=0,1) to
-# filter before results are received
+#    filter before results are received.
 #
 # Please document your decision and your reasons for it.
 #
+
+
+url = (f"{base_url}/routes?filter[type]=0,1"
+       f"&fields[route]=long_name"
+       f"&include=stop")
+
+r = requests.get(url)
+
+route_ids = []
+for e in r.json()["data"]:
+  route_ids.append(e["id"])
+
+routes_stops_dict = {}
+for route_id in route_ids:
+  url = f"{base_url}/stops?filter[route]={route_id}"
+  r = requests.get(url)
+  num_stops = len(r.json()["data"])
+  routes_stops_dict[route_id] = num_stops
+
+# Display min and max of stop count across routes
+max_stops = max(routes_stops_dict.values())
+min_stops = min(routes_stops_dict.values())
+for route, stops in routes_stops_dict.items():
+  if stops == max_stops:
+    print("Route %s has the most stops with %d stops." % (route, stops))
+  if stops == min_stops:
+    print("Route %s has the fewest stops with %d stops." % (route, stops))
+
 #####################################################################################################
 # Question 2
 #
@@ -44,6 +78,87 @@ from pprint import pprint
 # 3. A list of the stops that connect two or more subway routes along with the relevant route
 #    names for each of those stops.
 #
+
+# Build lookup tables of routes and stops by id => name
+all_route_ids = {}
+all_stop_ids = {}
+
+def get_all_route_ids():
+  url = (f"{base_url}/routes?"
+         f"&{api_key}"
+         f"&fields[route]=short_name,long_name"
+         f"&filter[type]=0,1")
+
+  r = requests.get(url)
+  for e in r.json()["data"]:
+    all_route_ids[e["id"]] = e["attributes"]["long_name"]
+  return all_route_ids
+
+def get_all_stop_ids():
+  url = (f"{base_url}/stops?"
+         f"&{api_key}"
+         f"&fields[stops]=short_name"
+         f"&filter[route_type]=0,1")
+
+  r = requests.get(url)
+  j = r.json()
+  for e in j["data"]:
+    all_stop_ids[e["id"]] = e["attributes"]["name"]
+  return all_stop_ids
+
+get_all_route_ids()
+get_all_stop_ids()
+
+######################################################################
+
+routes_stops_dict = {} 
+stops_routes_dict = {} 
+
+# Now gather all the trips (with route and stops included) by route id
+for route_id in all_route_ids.keys():
+  url = (f"{base_url}/trips?"
+         f"&{api_key}"
+         f"&include=route,stops"
+         f"&page[limit]=500"
+         f"&filter[route]={route_id}"
+         f"&fields[trip]=name"
+         f"&page[limit]=100")
+
+  r = requests.get(url)
+  j = r.json()
+
+  for trip in j["data"]:
+    for stop in trip["relationships"]["stops"]["data"]:
+
+      # Only proceed if the stop is on a route_type=0,1
+      if stop["id"] not in all_stop_ids:
+        continue
+
+      # Build dict's of { route => stops } and { stop => routes }
+      if route_id not in routes_stops_dict:
+        routes_stops_dict[route_id] = set()
+
+      routes_stops_dict[route_id].add((stop["id"], all_stop_ids[stop["id"]]))
+
+      if stop["id"] not in stops_routes_dict:
+        stops_routes_dict[stop["id"]] = set()
+
+      stops_routes_dict[stop["id"]].add((route_id, all_route_ids[route_id]))
+
+# print("routes_stops_dict")
+# pprint(routes_stops_dict)
+# print("stops_routes_dict")
+# pprint(stops_routes_dict)
+
+print("\nStops connecting two or more subway routes:\n")
+for stop_id,routes in stops_routes_dict.items():
+  if len(stops_routes_dict[stop_id]) > 1:
+    print("\tStop: %s, Routes: %s" %
+          (all_stop_ids[stop_id],
+           [v[1] for v in stops_routes_dict[stop_id]]))
+
+exit()
+
 #####################################################################################################
 # Question 3
 #
@@ -64,44 +179,5 @@ from pprint import pprint
 # could work for different and/or larger subway systems.
 #
 # How you handle input, represent train routes, and present output is your choice.
-
-
-qs = "?filter[type]=0,1&fields[route]=long_name&include=stop"
-url = "https://api-v3.mbta.com/routes" + qs
-r = requests.get(url)
-
-route_ids = []
-for e in r.json()["data"]:
-  route_ids.append(e["id"])
-
-routes_stops_dict = {}
-for route_id in route_ids:
-  url = "https://api-v3.mbta.com/stops?filter[route]=" + route_id
-  r = requests.get(url)
-  num_stops = len(r.json()["data"])
-  routes_stops_dict[route_id] = num_stops
-
-# Display min and max of stop count across routes
-max_stops = max(routes_stops_dict.values())
-min_stops = min(routes_stops_dict.values())
-for route, stops in routes_stops_dict.items():
-  if stops == max_stops:
-    print("Route %s has the most stops with %d stops." % (route, stops))
-  if stops == min_stops:
-    print("Route %s has the fewest stops with %d stops." % (route, stops))
-
-
-exit()
-
-x = json.loads(r.json(), object_hook=lambda d: SimpleNamespace(**d))
-pprint(x)
-
-exit()
-
-##########################################################################################
-
-url = "https://api-v3.mbta.com/stops?page[limit]=10&filter[route_type]=0,1"
-r = requests.get(url)
-print(r.json())
 
 
